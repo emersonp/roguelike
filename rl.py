587374150b966795b8850ba4bef8ae497c4917f9
Parker Harris Emerson <parker.emerson@gmail.com>
2014-08-16 14:22:33 -0700

    Added Objects
    Added objects / classes for items / objects, and colors, and tile map.

--- rl.py
+++ rl.py
@@ -1,10 +1,51 @@
 import libtcodpy as libtcod
 
-#actual size of the window
+#############################################
+# Constants and Big Vars
+#############################################
+
+# Size of the window
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 
-LIMIT_FPS = 20  #20 frames-per-second maximum
+# Size of the Map
+MAP_WIDTH = 80
+MAP_HEIGHT = 45
+
+# Colors of Terrain
+color_dark_wall = libtcod.Color(0, 0, 100)
+color_dark_ground = libtcod.Color(50, 50, 150)
+
+#############################################
+# Classes
+#############################################
+
+class Object:
+  # This object is a generic item in game: player, monster, item, tile feature
+  # An object is always represented as a symbol on screen.
+  def __init__(self, x, y, char, color):
+    self.x = x
+    self.y = y
+    self.char = char
+    self.color = color
+
+  def move(self, dx, dy):
+    # Move object by the param amount
+    self.x += dx
+    self.y += dy
+
+  def draw(self):
+    # Set the color and then draw the corresponding character of the object in that color.
+    libtcod.console_set_default_foreground(con, self.color)
+    libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+  def clear(self):
+    # Erase the character that represents this object
+    libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+#############################################
+# Functions
+#############################################
 
 def handle_keys():
   global playerx, playery
@@ -21,16 +62,13 @@ def handle_keys():
 
   #movement keys
   if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-    playery -= 1
-
+    player.move(0, -1)
   elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-    playery += 1
-
+    player.move(0, 1)
   elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-    playerx -= 1
-
+    player.move(-1, 0)
   elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-    playerx += 1
+    player.move(1, 0)
 
 #############################################
 # Initialization & Main Loop
@@ -38,17 +76,22 @@ def handle_keys():
 
 libtcod.console_set_custom_font(b'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'python/libtcod tutorial', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-playerx = SCREEN_WIDTH//2
-playery = SCREEN_HEIGHT//2
+player = Object(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH//2 - 5, SCREEN_HEIGHT//2, '@', libtcod.yellow)
+objects = [npc, player]
 
 while not libtcod.console_is_window_closed():
-  libtcod.console_set_default_foreground(0, libtcod.white)
-  libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+  # Draw all the objects in the object list
+  for object in objects:
+    object.draw()
 
+  libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
   libtcod.console_flush()
 
-  libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+  for object in objects:
+    object.clear()
 
   #handle keys and exit game if needed
   exit = handle_keys()
