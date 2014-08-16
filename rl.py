46f8759c4e7c76fef0805596acda584591264d97
Parker Harris Emerson <parker.emerson@gmail.com>
2014-08-16 15:01:05 -0700

    Movment and WALLS!
    Added walls and background tile color.

--- rl.py
+++ rl.py
@@ -16,6 +16,7 @@ MAP_HEIGHT = 45
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_dark_ground = libtcod.Color(50, 50, 150)
 
+map = []
 #############################################
 # Classes
 #############################################
@@ -42,6 +43,16 @@ class Object:
   def clear(self):
     # Erase the character that represents this object
     libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
+class Tile:
+  # A tile on the map
+  def __init__(self, blocked, block_sight = None):
+    self.blocked = blocked
+    # By default, if a tile is blocked, it also blocks sight.
+    if block_sight == None:
+      block_sight = blocked
+    self.block_sight = block_sight
+
 
 #############################################
 # Functions
@@ -70,6 +81,46 @@ def handle_keys():
   elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
     player.move(1, 0)
 
+def make_map():
+  global map
+
+  # Fill the map with "unblocked" tiles.
+  for x in range(MAP_WIDTH):
+    map.append([])
+    for y in range(MAP_HEIGHT):
+      map[x].append(Tile(False))
+
+  #map = [[ Tile(False)
+  #  for y in range(MAP_HEIGHT) ]
+  #    for x in range(MAP_WIDTH) ]
+
+  # Place to Test Pillars to test the map.
+  map[30][22].blocked = True
+  map[30][22].block_sight = True
+  map[40][22].blocked = True
+  map[40][22].block_sight = True
+
+def render_all():
+  global color_dark_ground
+  global color_dark_wall
+  global map
+
+  for y in range(MAP_HEIGHT):
+    for x in range(MAP_WIDTH):
+      wall = map[x][y].block_sight
+      if wall:
+        libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+      else:
+        libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+
+  # Draw all objects in the object list.
+  for object in objects:
+    object.draw()
+
+  # Blit the contents of 'con' to the root console.
+  libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+
+
 #############################################
 # Initialization & Main Loop
 #############################################
@@ -78,16 +129,22 @@ libtcod.console_set_custom_font(b'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE |
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'python/libtcod tutorial', False)
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 
+# Create the Player
 player = Object(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, '@', libtcod.white)
+
+# Create an NPC
 npc = Object(SCREEN_WIDTH//2 - 5, SCREEN_HEIGHT//2, '@', libtcod.yellow)
+
+# The List of Objects
 objects = [npc, player]
 
-while not libtcod.console_is_window_closed():
-  # Draw all the objects in the object list
-  for object in objects:
-    object.draw()
+# Make the Map
+make_map()
 
-  libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+while not libtcod.console_is_window_closed():
+  # Render the screen.
+  render_all()
+
   libtcod.console_flush()
 
   for object in objects:
